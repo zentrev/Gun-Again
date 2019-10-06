a6b24580a875c95ec314e76d7f76ab8b95b352ae
Jonathon Lau <Jonathon_lau@hotmail.com>
2019-10-06 10:09:37 -0600

    BossMovements

---
+++ Assets/Scripts/Agents/Boss/Boss_Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Movement : MonoBehaviour
+{
+    GameObject player = null;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(player != null)
+        {
+            if((transform.position - player.transform.position).magnitude < 100)
+            {
+                transform.position += transform.forward * 30 * Time.deltaTime;
+            }
+        }
+    }
+}
