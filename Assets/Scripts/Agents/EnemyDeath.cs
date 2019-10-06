543432ed75990cfd4c822d2f941ce6c2a3dcca4c
Kaedian <38064751+Kaedian-Maxfield@users.noreply.github.com>
2019-10-06 08:40:26 -0600

    more dying

--- Assets/Scripts/Agents/EnemyDeath.cs
+++ Assets/Scripts/Agents/EnemyDeath.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    public PathCreator[] paths;
     public PathCreator pathCreator;
     public EndOfPathInstruction end = EndOfPathInstruction.Stop;
     public float speed = 30;
     float dstTravelled;
     float totalDistance;
+    int pathChoice = -1;
     // Start is called before the first frame update
     public void Start()
     {
+        if(pathChoice != -1)
+        {
+            pathCreator = paths[pathChoice];
+        }
+        else
+        {
+            pathCreator = paths[0];
+        }
         pathCreator.gameObject.transform.parent = null;
         totalDistance = pathCreator.path.length;
     }
@@ -29,9 +39,10 @@ public class EnemyDeath : MonoBehaviour
         }
     }
 
-    public void Enable()
+    public void OnEnable(int path = 1)
     {
         enabled = true;
+        pathChoice = path;
     }
 
 }
