d9215af427631b33a73f24d67ae6b90773666f9e
Yoannyz <38017282+Yoannyz@users.noreply.github.com>
2019-10-06 02:54:34 -0600

    Messing with Turret

--- Assets/Scripts/Turret/Projectile.cs
+++ Assets/Scripts/Turret/Projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] [Range(0.0f, 10.0f)] float m_lifeTime = 5.0f;
-    [SerializeField] [Range(0.0f, 100.0f)] float m_speed = 50.0f;
+    [SerializeField] [Range(0.0f, 1000000.0f)] float m_speed = 50.0f;
 
     void Awake()
     {
